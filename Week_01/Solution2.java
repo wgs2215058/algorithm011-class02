06a8a0882655e5a9485cdb550d19b996cfed71a0
Geekuniversity <wangguoshen@126.com>
2020-06-27 19:20:57 +0800

    旋转数组

---
+++ Week_01/Solution2.java
@@ -0,0 +1,20 @@
+class Solution2 {
+    public void rotate(int[] nums, int k) {
+        int len  = nums.length;
+        k = k % len;
+        int count = 0;         
+        for(int start = 0; count < len; start++) {
+            int cur = start;      
+            int pre = nums[cur];   
+            do{
+                int next = (cur + k) % len;
+                int temp = nums[next];   
+                nums[next] = pre;
+                pre = temp;
+                cur = next;
+                count++;
+            }while(start != cur)  ;   
+             
+        }   
+    }
+}
